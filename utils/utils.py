b33f5aa0f59776b6260a1a6518c0e7c7a549a657
rainorangelemon <rainorangelemon@gmail.com>
2022-11-14 15:49:07 -0800

    RRT finished

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,18 @@
 import numpy as np
 from PIL import Image
 
+
+class DotDict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
+def create_dot_dict(**kwargs):
+    return DotDict(dict(**kwargs))    
+
+    
 def save_gif(imgs, gif_name, duration=50):
     # Setup the 4 dimensional array
     a_frames = []
@@ -10,3 +22,5 @@ def save_gif(imgs, gif_name, duration=50):
 
     ims = [Image.fromarray(a_frame) for a_frame in a]
     ims[0].save(gif_name, save_all=True, append_images=ims[1:], loop=0, duration=duration)
+    
+    
