0c3143dd7a8b812be0e4eb8c850f38edb2839ff0
rainorangelemon <rainorangelemon@gmail.com>
2022-12-21 14:09:20 -0800

    Something terribly misguided

--- objects/obstacle_generator.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class ObstacleGenerator(ABC):
-    @abstractmethod
-    def generate(self):
-        pass
--- robot/extend_kuka_robot.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-import numpy as np
-from robot.kuka_robot import KukaRobot
-import pybullet as p
-
-class ExtendKukaRobot(KukaRobot): # The kuka with 13 DoF
-    
-    def __init__(self, urdf_file="../data/robot/kuka_iiwa/model_3.urdf", collision_eps=0.5, **kwargs):
-        super(ExtendKukaRobot, self).__init__(urdf_file=urdf_file, collision_eps=collision_eps, **kwargs)   
